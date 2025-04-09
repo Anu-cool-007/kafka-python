e4abc709c904b1f3c46142e6931ada71787a7325
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2025-04-09 11:27:26 +0530

    added corr id to response

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import socket  # noqa: F401
-
+from uuid import uuid4
 
 def main():
     # You can use print statements as follows for debugging,
@@ -9,7 +9,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    connection, address = server.accept() # wait for client
+
+    corelation_id = str(uuid4().int & (1<<32)-1)
+    message_size = len(corelation_id)
+    message = f"{message_size}\r\n{corelation_id}"
+    connection.sendall(message.encode())
 
 
 if __name__ == "__main__":
